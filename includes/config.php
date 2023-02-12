8aa13198e7aa914a3736c64f2c0311084df8728d
kitwa <dominique@lucidocean.com>
2023-02-12 20:35:50 +0200

    change config

--- includes/config.php
+++ includes/config.php
@@ -17,8 +17,7 @@ if ($_SERVER['HTTP_HOST'] == "localhost" || $_SERVER['HTTP_HOST'] == "localhost:
 } else {
     try {
 
-        $con = new PDO("mysql:dbname=crijagency;host=localhost", "agency", "Agency@1502");
-        // $con = new PDO("mysql:dbname=agencytest;host=localhost", "agencytest", "Test@2021");
+        $con = new PDO("mysql:dbname=dollaradam;host=localhost", "agency", "Agency@1502");
 
         $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
     } catch (PDOException $e) {
@@ -26,27 +25,15 @@ if ($_SERVER['HTTP_HOST'] == "localhost" || $_SERVER['HTTP_HOST'] == "localhost:
     }
 }
 
-// drop user 'agency'@'localhost';
-
-// // mysqldump -h localhost -u root -p agency > backup_db.sql
-
-// mysqldump -h localhost -u agency -p crijagency > backup_db.sql
 
 
 
-// CREATE USER 'agency'@'localhost' IDENTIFIED BY 'Agency@1502';
+
+// CREATE USER 'dollaradam'@'localhost' IDENTIFIED BY 'Agency@1502';
 
 // GRANT ALL PRIVILEGES ON * . * TO 'agency'@'localhost';
 
 // FLUSH PRIVILEGES;
-
-// CREATE USER 'agencytest'@'localhost' IDENTIFIED BY 'Test@2021';
-
-// GRANT ALL PRIVILEGES ON * . * TO 'agencytest'@'localhost';
-
-// FLUSH PRIVILEGES;
-
-// UPDATE `users` SET `password`='9c1a3a68f67af35c0f4ed34c8f87b241' WHERE `id`='1';
 
 
 /******  Instruction to reset  *****/
